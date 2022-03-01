7cf8ca1d66c31ac2a109676f80cdbae4e9a50f03
papajonel05@gmail.com <pereyratapia>
2022-03-01 16:36:37 +0800

    first commit

--- JackNPoy/main.js
+++ JackNPoy/main.js
@@ -1,3 +1,12 @@
 const computerDisplay = document.getElementById('computer-choice');
 const userDisplay = document.getElementById('user-choice');
 const resultDisplay = document.getElementById('result');
+const choices = document.querySelectorAll('button');
+
+let userChoice;
+let computerChoice;
+
+choices.forEach(choice => choice.addEventListener('click', (e) => {
+    userChoice = e.target.id;
+    userDisplay.textContent = " User choice: " + userChoice;
+}))
