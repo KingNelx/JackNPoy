1df034657d686c5ff94ee67d7dabe632c51431e6
papajonel05@gmail.com <pereyratapia>
2022-03-01 15:04:36 +0800

    update logic

--- JackNPoy/main.js
+++ JackNPoy/main.js
@@ -4,10 +4,24 @@ const result = document.getElementById('result');
 const choices = document.querySelectorAll('button');
 
 let choiceByUser
-
+let computerChoices
 choices.forEach(picks => picks.addEventListener('click', (e) => {
     choiceByUser = e.target.id
     userChoice.textContent = choiceByUser
+    generateComputerChoice()
 }))
 
 
+
+function generateComputerChoice(){
+    let randomChouice = Math.floor(Math.random() * 3)
+    if(randomChouice == 0){
+        computerChoices = 'rock'
+    }else if(randomChouice == 1){
+        computerChoices = 'paper'
+    }
+    else{
+        computerChoices = 'scissors'
+    }
+    computerChoice.textContent = computerChoices
+}
