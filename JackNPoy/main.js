232aff9388bcd1e85e36f1bad7b58f5564b3b781
papajonel05@gmail.com <pereyratapia>
2022-03-01 15:37:33 +0800

    first commit

---
+++ JackNPoy/main.js
@@ -0,0 +1,3 @@
+const computerDisplay = document.getElementById('computer-choice');
+const userDisplay = document.getElementById('user-choice');
+const resultDisplay = document.getElementById('result');
