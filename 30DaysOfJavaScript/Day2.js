18e581a39dfc9c29f1a49960301464400caa8e1a
KingNelxV2 <88817315+KingNelx@users.noreply.github.com>
2022-03-10 16:17:15 +0800

    Update Day2.js

--- 30DaysOfJavaScript/Day2.js
+++ 30DaysOfJavaScript/Day2.js
@@ -3,5 +3,15 @@
 // There are 2 ways of declaring arrays
 
 let myArray = new Array();
-myArray.push("Jonel")
-console.log(myArray)
+
+let emptyArray = []
+
+// pushin elements in the array
+
+let thisEmptyArray = new Array();
+thisEmptyArray.push("Jonel")
+thisEmptyArray.push("Pogi")
+thisEmptyArray.push("Talaga")
+
+console.log(thisEmptyArray)
+
