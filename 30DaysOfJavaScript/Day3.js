738a7b6f205de0ee9854fbdbf3e109448c54567f
KingNelxV2 <88817315+KingNelx@users.noreply.github.com>
2022-03-12 21:23:09 +0800

    Update Day3.js

--- 30DaysOfJavaScript/Day3.js
+++ 30DaysOfJavaScript/Day3.js
@@ -12,7 +12,14 @@ const employee = {
     "Favorite Game": "Dota"
 }
 
+// we can not acess the multiword properties with doc notation
+// employee.Favorite Game == error
+// employee["First Game"] == success
 
 for(let key in employee){
     console.log(employee[key])
 }
+
+const anotherEmployee = {};
+anotherEmployee["Software Engineer"] = true
+console.log(anotherEmployee)
