4c0be5cbccb8a0335a36350ac2f68b652cd26455
KingNelxV2 <88817315+KingNelx@users.noreply.github.com>
2022-03-12 21:19:20 +0800

    Update Day3.js

--- 30DaysOfJavaScript/Day3.js
+++ 30DaysOfJavaScript/Day3.js
@@ -1,9 +1,17 @@
+/*
+objects are used to store keyed collections of 
+various data and more complex entities.
+*/
+
 const employee = {
     firstName: "Jonel",
     lastName: "Tapia",
     Age: 21,
-    Dream: "Software Engineer"
+    Dream: "Software Engineer",
+    isGrinding: true,
+    "Favorite Game": "Dota"
 }
+
 
 for(let key in employee){
     console.log(employee[key])
